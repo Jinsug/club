8a5bbe17331024169732027eb2af1781cc16cdf9
w1441639547 <1441639547@qq.com>
2018-05-03 12:08:12 +0800

    修改首页
    2018-05-03 12:08

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,12 @@
 //获取应用实例
 Page({
   data: {
-    base_picture_url: 'https://www.ecartoon.com.cn/picture'
+    base_picture_url: 'https://www.ecartoon.com.cn/picture',
+    tickets: [
+      { price: 90, contant: '满1000元使用'},
+      { price: 200, contant: '满2000元使用'},
+      { price: 300, contant: '满3000元使用'}
+    ]
   },
   onLoad: function () {
     
