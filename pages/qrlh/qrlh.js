7b9137489265c5d42010cf636bc744d739f1ada4
w1441639547 <1441639547@qq.com>
2018-05-23 19:00:03 +0800

    添加请人流汗页面
    2018-05-23 18:59

--- pages/qrlh/qrlh.js
+++ pages/qrlh/qrlh.js
@@ -4,7 +4,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    cardList: [],
+    selectIndex: 0,
+    selectImage: '201805231658.png',
+    UnSelectedImage: '201805231659.png'
   },
 
   /**
@@ -26,6 +29,7 @@ Page({
    */
   onShow: function () {
     
+    this.methods.getClubData(this);
   },
 
   /**
@@ -54,5 +58,55 @@ Page({
    */
   onReachBottom: function () {
     
+  },
+
+  /**
+   * wxml绑定函数: 商品列表点击绑定
+   */
+  selectProduct: function (e) {
+    var index = e.currentTarget.dataset.index;
+    var cardList = this.methods.clearSelected(this.data.cardList, this);
+    cardList[index].selectImage = this.data.selectImage;
+    this.setData({
+      index: index,
+      cardList: cardList
+    });
+  },
+
+  /**
+   * 自定义函数
+   */
+  methods: {
+    // 加载俱乐部数据
+    getClubData: function (obj) {
+      wx.request({
+        url: 'https://www.ecartoon.com.cn/clubmp!findClubById.asp',
+        data: {
+          id: wx.getStorageSync('clubId')
+        },
+        success: function (res) {
+          var cardList = obj.methods.clearSelected(res.data.cardList, obj);
+          // 选中时显示的图片
+          cardList[obj.data.selectIndex].selectImage = obj.data.selectImage;
+          obj.setData({
+            cardList: cardList
+          });
+        }
+      });
+    },
+
+    /**
+     * 清空选中状态
+     */
+    clearSelected: function (cardList, obj) {
+      // 重置为默认图片
+      var resetDefaultImage = function (card) {
+        // 未选中显示图片
+        card.selectImage = obj.data.UnSelectedImage;
+        return card;
+      }
+      return cardList.map(resetDefaultImage);
+    }
+
   }
 })
