5a7cb81a20cec484d09c454e9dbaad6458110048
w1441639547 <1441639547@qq.com>
2018-08-08 16:25:23 +0800

    2018-08-08 16:25

--- pages/planProduct/planProduct.js
+++ pages/planProduct/planProduct.js
@@ -1,61 +1,113 @@
+var util = require('../../utils/util.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    planUrl: ''
+    plan: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var url = 'https://www.ecartoon.com.cn/miniProgram/coach/plan.jsp?planId=' + options.planId + '&type=club';
-    this.setData({
-      planUrl: url
-    });
+    // 初始化
+    this.methods.init(options.planId, this);
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
+  },
+
+  // 用户选择改变日期
+  changeDate: function (e) {
+    var plan = this.data.plan;
+    plan.currentDate = e.detail.value;
+    this.setData({
+      plan: plan
+    });
+  },
+
+  // 用户点击购买按钮
+  goBuy: function () {
+    var param = {
+      productId: this.data.plan.id,
+      productName: this.data.plan.name,
+      productPrice: this.data.plan.price,
+      image: this.data.plan.image1,
+      productType: 'plan',
+      time: this.data.plan.currentDate
+    }
+    var plan_data = encodeURI(JSON.stringify(param));
+    wx.navigateTo({
+      url: `../order/order?product=${plan_data}`
+    });
+  },
+
+  /**
+   * 自定义函数
+   */
+  methods: {
+    /**
+     * 初始化
+     */
+    init: function (planId, obj) {
+      wx.request({
+        url: 'https://www.ecartoon.com.cn/coachmp!loadPlan.asp',
+        data: {
+          id: planId,
+          type: 3
+        },
+        success: function (res) {
+          var plan = res.data.item;
+          plan.currentDate = util.formatTime(new Date());
+          plan.planTypes = { 'A': '瘦身减重', 'B': '健美增肌', 'C': '运动康复', 'D': '提高运动表现'}
+          plan.applyObjects = { 'A': '初级', 'B': '中级', 'C': '高级'}
+          plan.scenes = { 'A': '办公室', 'B': '健身房', 'C': '家庭', 'D': '户外'}
+          obj.setData({
+            plan: plan
+          });
+        }
+      })
+    }
   }
 })
--- pages/privateProduct/privateProduct.js
+++ pages/privateProduct/privateProduct.js
@@ -1,21 +1,19 @@
+var util = require('../../utils/util.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    privateUrl: ''
+    product: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var url = 'https://www.ecartoon.com.cn/miniProgram/coach/private.jsp?productId=' +  
-      options.productId + '&type=club';
-    this.setData({
-      privateUrl: url
-    });
+    // 初始化
+    this.methods.init(options.productId, this);
   },
 
   /**
@@ -58,5 +56,54 @@ Page({
    */
   onReachBottom: function () {
     
+  },
+
+  // 用户选择改变日期
+  changeDate: function (e) {
+    var product = this.data.product;
+    product.currentDate = e.detail.value;
+    this.setData({
+      product: product
+    });
+  },
+
+  // 用户点击购买按钮
+  goBuy: function () {
+    var param = {
+      productId: this.data.product.id,
+      productName: this.data.product.name,
+      productPrice: this.data.product.price,
+      image: this.data.product.image,
+      productType: 'product',
+      time: this.data.product.currentDate
+    }
+    var product_data = encodeURI(JSON.stringify(param));
+    wx.navigateTo({
+      url: `../order/order?product=${product_data}`
+    });
+  },
+
+  /**
+   * 自定义函数
+   */
+  methods: {
+    /**
+     * 初始化
+     */
+    init: function (productId ,obj) {
+      wx.request({
+        url: 'https://www.ecartoon.com.cn/coachmp!getPrivateInfo.asp',
+        data: {
+          id: productId
+        },
+        success: function (res) {
+          var product = res.data;
+          product.currentDate = util.formatTime(new Date());
+          obj.setData({
+            product: product
+          });
+        }
+      })
+    }
   }
 })
